d7695d77f668d1a44e6f909e09224a26af836567
Peter Kim <petekim@gmail.com>
2022-03-31 14:49:12 -0700

    Update pd_sns_plt.py

--- pd_sns_plt.py
+++ pd_sns_plt.py
@@ -25,8 +25,19 @@ plt.title(f"`{x}` vs. `{y}` with hue of `{hue}`")
 # Rotates X-Axis Ticks by 90-degrees
 plt.xticks(rotation = 90) 
 
-# Put the legend out of the figure
-plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+# https://matplotlib.org/1.3.1/users/legend_guide.html
+# reorder legend
+flag_reorder = False
+if flag_reorder:
+  handles1, labels1 = plt.gca().get_legend_handles_labels()
+  print("Original label order: ", labels1)
+  order = [1, 0, 2]
+  handles2, labels2 = ([handles1[idx] for idx in order], [labels1[idx] for idx in order])
+  print("Adjusted label order: ", labels1)
+  plt.legend(handles2, labels2, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+else:
+  # Put the legend out of the figure
+  plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 
 plt.show()
 print()
